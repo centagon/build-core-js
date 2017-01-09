af6d985e49554daa59e71988e7d2becf8e5cc8c6
René Heijnemans <rene.heijnemans@centagon.com>
2017-01-09 15:25:10 +0100

    Sidebar Resource updated

--- src/js/lib/Sidebar.js
+++ src/js/lib/Sidebar.js
@@ -1,5 +1,11 @@
 class Sidebar {
 
+    setHeaders(xhr) {
+        
+        xhr.setRequestHeader('X-CSRF-TOKEN', config.csrf_token);
+        
+    }
+    
     constructor() {
         const self = this;
         const doc = $(document);
@@ -37,8 +43,78 @@ class Sidebar {
     }
 
     close(sidebar) {
-        sidebar.removeClass('sidebar--open').find('.content').html('');
+        sidebar 
+                .off('click','.button--secondary')
+                .off('submit','form')
+                .removeClass('sidebar--open')
+                .find('.content').html('');
     }
+    
+    resource(sidebar, url, params) {
+        const $sidebar = $(sidebar);
+        
+        this.close( $sidebar );
+        
+        var P = $.Deferred();
+
+        this.load( $sidebar, url).then( () => {
+            $(sidebar)
+                .on('submit', 'form', (e) => {
+                    e.preventDefault();
+            
+                    const $form = $(e.target);
+                    
+                    // Clear old errors
+                    $form.find('.error[for]').removeClass('error');
+                    
+                    var obj = $(e.target).serializeArray();
+                    var arr = {};
+                    
+                    const action = $form.attr('action');
+                    var method = $form.attr('method');
+                    
+                    _.each(obj, (o) => {
+                        arr[o.name] = o.value;
+                    });
+                    
+                    $.ajax({
+                        url: action,
+                        beforeSend: this.setHeaders,
+                        type: method,
+                        data: arr,
+                        dataType: 'json'
+                    }).then( (response) => {
+                        P.resolve(response);
+                        this.close($sidebar);
+                    }).fail( (response) => {
+                        
+                        if (!response.responseJSON) {
+                            P.reject(response);
+                            this.close($sidebar);
+                            return;
+                        }
+                        
+                        _.each( response.responseJSON , (elem, i) => {
+                            const $row = $form.find(`.row[for=${i}]`);
+                            $row.addClass('error');
+                            $row.find(`.form-error[for=${i}]`).html(elem[0]);
+                        });
+                        
+                    });
+
+                })
+                .on('click', '.button--secondary', (e) => {
+                    P.reject();
+            
+                    e.preventDefault();
+
+                    this.close($sidebar);
+                });
+
+        });
+
+    }
+    
 }
 
 export default Sidebar;
